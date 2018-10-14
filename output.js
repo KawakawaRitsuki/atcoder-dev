b3606ea952aa9b3c8372884dfec341c71abf5ade
Ritsuki Goto <develop@mizucoffee.net>
2018-10-15 01:39:31 +0900

    [update] template

--- output.js
+++ output.js
@@ -1,19 +1,35 @@
 'use strict';
 
-var hasN = true;
-// p1: 2D split
-// p2: space split
-// p3: line split
-function main(p1, p2, p3, so) {
+var hasN = true; // has N only line
+var type = "a";
+// a: 2D split
+// b: space split
+// c: line split
+// z: other (only source)
+function main(N, p, s) {
   console.log();
 }
 function common(a) {
   var line = a.split('\n').slice(0, a.split('\n').length - 1);
-  var p2 = line[0].split(' ');
-  if (hasN) line = line.slice(1);
-  var p1 = line.map(function (e) {
-    return e.split(' ');
-  });
-  main(p1, p2, line, a);
+  if (hasN) var N = Number(line.shift());
+  switch (type) {
+    case "a":
+      main(N, line.map(function (e) {
+        return e.split(' ').map(function (ee) {
+          return Number(ee);
+        });
+      }), a);
+      break;
+    case "b":
+      main(N, line[0].split(' ').map(function (e) {
+        return Number(e);
+      }), a);
+      break;
+    case "c":
+      main(N, line, a);
+      break;
+    case "z":
+      main(N, null, a);
+  }
 }
 common(require("fs").readFileSync("/dev/stdin", "utf8"));
